246d3446fdccf7cf42110c5d35cdc69c2f7962b1
Marco Bazzani <marcobazzani@users.noreply.github.com>
2026-04-22 22:30:32 +0200

    Update nightly to nightly-c981fbe

--- Casks/financecopilot-nightly.rb
+++ Casks/financecopilot-nightly.rb
@@ -1,5 +1,5 @@
 cask "financecopilot-nightly" do
-  version "nightly-684e79f"
+  version "nightly-c981fbe"
   sha256 :no_check
 
   url "https://github.com/marcobazzani/FinanceCopilot/releases/download/latest/FinanceCopilot-macos.dmg"
